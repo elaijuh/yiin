51937484df41a8a0758657552976694a1fdf4f6d
elaijuh <hjiale@gmail.com>
2017-03-09 03:33:13 +0800

    x-real-ip

--- routes/posts.js
+++ routes/posts.js
@@ -13,7 +13,7 @@ marked.setOptions({
 /* GET users listing. */
 router.post('/', function(req, res, next) {
   let post = new Post({
-    ip: req.headers['X-Real-IP'],
+    ip: req.headers['x-real-ip'],
     user_agent: req.headers['user-agent'],
     referer: req.headers['referer'],
     post: req.body.post 
